5dd9bb09a1684063fe107eb7e965520ed13f2b3f
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 13:37:54 -0500

    setup react router dom

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './Header'
 import BinsList from './bins/BinsList'
+import BinsMain from './bins/BinsMain'
 
-export default () => {
+export default props => {
   return (
     <div>
       <Header />
-      <BinsList />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={BinsList} />
+          <Route exact path="/bins" component={BinsMain} />
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
