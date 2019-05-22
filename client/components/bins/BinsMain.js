c63410d6a476a2fa381ce093cf88691e185209b8
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 14:09:49 -0500

    created code editor with code mirror

--- client/components/bins/BinsMain.js
+++ client/components/bins/BinsMain.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { withTracker } from 'meteor/react-meteor-data'
+import BinsEditor from './BinsEditor'
+import { Bins } from '../../../imports/collections/Bins'
 
 class BinsMain extends Component {
   render() {
     return (
-      <div>BinsMain</div>
+      <div>
+        <BinsEditor bin={this.props.bin} />
+      </div>
     )
   }
 }
 
-export default BinsMain
+export default withTracker(props => {
+  const { binId } = props.match.params
+  Meteor.subscribe('bins')
+  return { bin: Bins.findOne(binId) }
+})(BinsMain)
