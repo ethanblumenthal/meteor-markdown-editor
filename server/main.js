a10ccc88c4afbc0d7d5ad09eafb76a469b16f29c
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-05-22 12:37:40 -0500

    created meteor method to create bins

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor'
+import Bins from '../imports/collections/Bins'
 
 Meteor.startup(() => {
   // code to run on server at startup
-});
+})
